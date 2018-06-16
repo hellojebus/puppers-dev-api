307bf9e24712c31a6aa32e69eb5e064659d9a384
Jesus Lizama <dev@hellojeb.us>
2018-06-15 17:01:02 -0700

    add pups model

--- app/routes/dev.php
+++ app/routes/dev.php
@@ -1,10 +1,23 @@
 <?php
 
 use Puppers\User;
+use Puppers\Pup;
 
 $app->get('/dev', function($req, $res, $args){
 	$users = User::all();
 	return $res->withStatus(200)->withJson($users);
+});
+
+
+$app->get('/dev/dogs', function($req, $res, $args){
+	$pups = Pup::all();
+	return $res->withStatus(200)->withJson($pups);
+});
+
+$app->get('/dev/dogs/{pup_id}', function($req, $res, $args){
+	$pupId = $args["pup_id"];
+	$pup = Pup::find($pupId);
+	return $res->withStatus(200)->withJson($pup);
 });
 
 ?>
